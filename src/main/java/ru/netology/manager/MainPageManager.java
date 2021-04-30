a3d2792db499bb9c13fe1f8f94e3e7e06a7f7117
Max Vertlib <maximvert@gmail.com>
2021-04-30 11:32:10 +0300

    v2

--- src/main/java/ru/netology/manager/MainPageManager.java
+++ src/main/java/ru/netology/manager/MainPageManager.java
@@ -3,11 +3,12 @@ package ru.netology.manager;
 import ru.netology.domain.Post;
 
 public class MainPageManager {
-
+    private Post[] post;
     public Post[] search(int ownerId,  String domain, boolean ownersOnly, int count, int offset){
-    return null;
+    return post;
     }
     public void delete(int ownerId, int postId) {
+
 
     }
 
